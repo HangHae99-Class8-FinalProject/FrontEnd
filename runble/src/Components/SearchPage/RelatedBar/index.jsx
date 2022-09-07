e1feaa3c7d95384cb60d633f8e4a37e9d3e0d96f
Dae-une <ko301201@naver.com>
2022-09-07 18:30:54 +0900

    [feat] 검색 기능 구현 및 검색어디바운싱

--- runble/src/Components/SearchPage/RelatedBar/index.jsx
+++ runble/src/Components/SearchPage/RelatedBar/index.jsx
@@ -1,29 +1,21 @@
 import React from "react";
 import { instance } from "../../../Utils/Instance";
 import { useQuery } from "react-query";
-import useQueryDebounce from "../../../Utils/useQueryDebounce";
+import useQueryDebounce from "../../../Hooks/useQueryDebounce";
 
 const RelatedBar = ({ searchTag }) => {
-  console.log("SearchTag:", searchTag);
-
   const debounceSearch = useQueryDebounce(searchTag, 500);
 
   const getRelated = async () => {
     const { data } = await instance.get(
       `http://54.167.169.43/api/post/autoSearch/?hashtag=${debounceSearch}`
     );
-    console.log("fecthData:", data);
     return data;
   };
 
-  console.log(debounceSearch);
-
   const { data: related } = useQuery(debounceSearch, getRelated, {
-    enabled: !!debounceSearch,
-    staleTime: 0
+    enabled: !!debounceSearch
   });
-
-  console.log("queryData:", related);
 
   return (
     <>
